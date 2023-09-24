5202e6c47c224f9b2997dc5b02adf647291aec02
ArkapravoRoy <125896104+ArkapravoRoy@users.noreply.github.com>
2023-09-25 00:03:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var s=document.querySelector(".sett")
+var d=document.getElementById("dark");
+function Abc(){
+    s.classList.toggle("sett-h");
+}
+d.onclick=function(){
+    d.classList.toggle("dark-oon");
+    
+}
